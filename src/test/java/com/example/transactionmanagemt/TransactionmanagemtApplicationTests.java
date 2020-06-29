d48bc47d490a7e444df462d4f20a7a5480320a29
ThontiRavindhar <ravindharthonti@gmail.com>
2020-06-29 11:34:34 +0530

    Sample maven hibernate example

---
+++ src/test/java/com/example/transactionmanagemt/TransactionmanagemtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transactionmanagemt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionmanagemtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
